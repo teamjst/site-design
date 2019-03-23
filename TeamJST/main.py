72f7c57fc903fdfbdb6697679dbef4ed80864f87
josefinafortin <josefina.l.fortin@gmail.com>
2019-03-23 02:58:17 -1000

    Add files via upload

--- TeamJST/main.py
+++ TeamJST/main.py
@@ -14,5 +14,34 @@ def about():
 def login():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/userhome")
+def userhome():
+    return render_template("userhome.html")
+
+@app.route("/lightsetting")
+def lightsetting():
+    return render_template("lightsetting.html")
+
+@app.route("/sound")
+def sound():
+    return render_template("sound.html")
+
+@app.route("/timer")
+def timer():
+    return render_template("timer.html")
+
+@app.route("/addDevice")
+def addDevice():
+    return render_template("addDevice.html")
+
+import webbrowser
+
+webbrowser.open('http://google.com')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
